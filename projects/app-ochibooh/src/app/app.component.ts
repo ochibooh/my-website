b2cbcd76b76c226ad701f342ec888af8afe1a4db
Phelix Ochieng <9phelix1@gmail.com>
2020-02-07 06:05:45 +0300

    adding store and utils libraries to app-ochibooh

--- projects/app-ochibooh/src/app/app.component.ts
+++ projects/app-ochibooh/src/app/app.component.ts
@@ -17,10 +17,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'app-ochibooh';
+    title = 'app-ochibooh';
 }
